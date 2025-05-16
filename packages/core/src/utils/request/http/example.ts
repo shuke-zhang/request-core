3e37f933ea0c638b8d062cdd9d51bf3aa7eba721
shuke <zhangenjian207@gmail.com>
2025-05-16 14:43:01 +0800

    优化

--- packages/core/src/utils/request/http/example.ts
+++ packages/core/src/utils/request/http/example.ts
@@ -1,152 +1,24 @@
-import type { AxiosRequestConfig, Canceler } from 'axios'
-import type { HttpRequestConfig } from './http-types'
-import axios from 'axios'
-import { cancelInterceptor, generateKey, getSystemErrorMessage, handleError } from '../helpers'
-import { ContentTypeEnum, RequestMethodsEnum } from '../shared'
-import { HttpRequest } from './http'
+import { useHttpRequest } from './hooks'
 
-interface userCustomConfig {
-  /**
-   * @description 是否需要携带token
-   */
-  withToken?: boolean
-  /**
-   * @description 是否添加时间戳
-   */
-  joinTime?: boolean
-  /**
-   * @description 是否提示错误信息
-   */
-  showErrorMsg?: boolean
-  /**
-   * @description 是否提示请求拦截器错误信息 - 弹窗方式
-   */
-  isShowRequestErrorMsg?: boolean
-  /**
-   * @description 忽略重复请求。第一个请求未完成时进行第二个请求，第一个会被被取消
-   *              参考 axios 取消请求 https://axios-http.com/zh/docs/cancellation
-   */
-  ignoreRepeatRequest?: boolean
-  /**
-   * 下载文件名称
-   */
-  filename?: string
-};
+function getToken() {
+  console.log('getToken')
+  return 'token'
+}
+const { request } = useHttpRequest(
+  {
+    baseURL: 'http://vue.ruoyi.vip',
+    withToken: false,
+  },
+  { getToken },
+)
 
-interface userRequestOptions extends AxiosRequestConfig, userCustomConfig {}
-
-interface userRequestConfig {
-  getToken: () => string | null
-  tokenKey?: string
-  tokenKeyScheme?: string
+function getCode() {
+  return request.get<{ img: string, uuid: string }>({
+    url: '/prod-api/captchaImage',
+    withToken: false,
+  })
 }
 
-interface userToolMethodConfig {
-  showMessageSuccess?: (message: string) => void
-  showMessageError?: (message: string) => void
-  toAuth: () => void
-  requestError?: (error: any) => (Promise<any> | any)
-}
-
-type ResponseResult<T extends object = object> = {
-  code: number
-  status: number
-  msg: string
-  message: string
-} & T
-
-const cancelMap = new Map<string, Canceler>()
-
-export function httpRequest(options: userRequestOptions, userRequestOptionConfig: userRequestConfig, userMethodConfig?: userToolMethodConfig) {
-  const request = new HttpRequest<userCustomConfig>(
-    {
-      baseURL: options.baseURL || 'http://vue.ruoyi.vip',
-      timeout: options.timeout || 3000 * 10,
-      headers: options.headers || {
-        'Content-Type': ContentTypeEnum.JSON,
-      },
-      onUploadProgress: (progressEvent) => {
-        return options.onDownloadProgress?.(progressEvent)
-        // 上传进度
-      },
-      onDownloadProgress(progressEvent) {
-        if (progressEvent.total) {
-          const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100)
-          console.log(`Download Progress: ${progress}%`)
-          options.onUploadProgress?.(progressEvent)
-        }
-      },
-      withToken: options.withToken || true,
-      joinTime: options.joinTime || true,
-      ignoreRepeatRequest: options.ignoreRepeatRequest || true,
-      showErrorMsg: options.showErrorMsg || true,
-    },
-    {
-      // 请求拦截器
-      request(config) {
-        /**
-         * token
-         */
-        const token = userRequestOptionConfig.getToken()
-        if (config?.withToken && token) {
-          config.headers![userRequestOptionConfig.tokenKey || 'Authorization'] = `${userRequestOptionConfig.tokenKeyScheme || 'Bearer'} ${token}`
-        }
-        /**
-         * 忽略重复请求。第一个请求未完成时进行第二个请求，第一个会被被取消
-         */
-        if (config.ignoreRepeatRequest) {
-          const key = generateKey({ ...config })
-          const cancelToken = new axios.CancelToken(c => cancelInterceptor(key, c, cancelMap)) // 创建一个取消 token
-          config.cancelToken = cancelToken
-        }
-        /**
-         * 添加时间戳到 get 请求
-         */
-        if (config.method?.toUpperCase() === RequestMethodsEnum.GET) {
-          config.params = { _t: `${Date.now()}`, ...config.params }
-        }
-
-        return config
-      },
-      // 请求拦截器错误
-      requestError(e) {
-        // 处理请求错误
-        userMethodConfig?.requestError?.(e) || Promise.reject(e)
-      },
-      // 相应拦截器
-      async response(_response) {
-        cancelMap.delete(generateKey(_response.config))
-        const config = _response.config as HttpRequestConfig<userCustomConfig>
-        // 返回原生响应
-        if (config.getResponse) {
-          return _response
-        }
-        const responseData = _response.data as ResponseResult<object>
-
-        if (responseData.code === 200) {
-          return responseData as any
-        }
-
-        if (responseData.code === 401) {
-          // 返回登录页
-          userMethodConfig?.toAuth()
-        }
-
-        const msg = responseData.msg || getSystemErrorMessage(responseData.code)
-        return handleError(msg, userMethodConfig?.showMessageError || (() => {
-          console.log(msg)
-        }), responseData.code !== 401 && !config?.showErrorMsg)
-      },
-      // 响应拦截器错误
-      responseError(error: any) {
-        if (error) {
-          const err = error?.errMsg || error?.msg || error?.message || ''
-          return handleError(err, userMethodConfig?.showMessageError || (() =>
-            console.log(err)))
-        }
-      },
-    },
-  )
-
-  return request
-}
+getCode().then((res) => {
+  console.log('调用成功', res.uuid, res.img)
+})
